5411cd7331302b593226974326f8b47e69744c92
Colt32905 <cbradle3@unca.edu>
2023-10-18 22:58:07 -0400

    hw5

---
+++ lectures/lecture14/02-style-property-demo/main.js
@@ -0,0 +1,35 @@
+
+function makeRed() {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+};
+
+function makeBlue() {
+    // your code here...
+    document.querySelector('#section2').style.backgroundColor = 'blue';
+    console.log('Change background to blue');
+};
+
+function makePink() {
+    // your code here...
+    document.querySelector('#section3').style.backgroundColor = 'pink';
+    console.log('Change background to pink');
+};
+
+function makeOrange() {
+    // your code here...
+    document.querySelector('#section4').style.backgroundColor = 'orange';
+    console.log('Change background to orange');
+}
+
+function makeWhite() {
+    document.querySelector("#section1").style.backgroundColor = 'white';
+    document.querySelector("#section2").style.backgroundColor = 'white';
+    document.querySelector("#section3").style.backgroundColor = 'white';
+    document.querySelector("#section4").style.backgroundColor = 'white';
+
+
+
+}
+
---
+++ lectures/lecture14/demo/greeting.js
@@ -0,0 +1,10 @@
+function sayHello() {
+    // your code here...
+document.querySelector("#message").innerHTML='oh hey';
+document.querySelector("#message").style.color="red";
+}
+
+function sayGoodbye() {
+    // your code here...
+document.querySelector("#message").innerHTML='adios';
+}
