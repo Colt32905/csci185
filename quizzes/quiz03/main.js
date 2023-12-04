bd844ebe4efff25057a170f20a7a788f6f3dcf0d
Colt32905 <cbradle3@unca.edu>
2023-12-04 16:28:24 -0500

    quiz3

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,24 @@
+function color1() {
+    document.getElementById("square1").style.backgroundColor="blue";
+}
+
+function color2() {
+    document.getElementById("square2").style.backgroundColor="yellow";
+}
+
+function color3() {
+    document.getElementById("square3").style.backgroundColor="green";
+}
+
+function color4() {
+    document.getElementById("square4").style.backgroundColor="purple";
+}
+
+function color5() {
+    document.getElementById("square5").style.backgroundColor="red";
+}
+
+function color6() {
+
+    document.getElementById("square6").style.backgroundColor="black";
+}
